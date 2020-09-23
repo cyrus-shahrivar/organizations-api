d83735ec817ea28f164e7daf681a5273fd485b31
cyrus-shahrivar <cyrus.shahrivar@gmail.com>
2020-09-22 22:19:54 -0400

    init commit
    - add express for node server framework
    - add basic GET and POST /organizations support
    - add manual rest client tests via vscode extension (Rest Client)
    - add mock database prior to real database integration

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// TODO: hook-up pg database:
+const database = [
+    {
+        name: 'ABC123 Corp.',
+        started: '2020-09-30T01:21:33.573Z',
+        employees: 12,
+        public: true
+    },
+    {
+        name: 'ABC123 Corp.',
+        started: '2020-09-24T01:21:33.573Z',
+        employees: 20,
+        public: true
+    },
+    {
+        name: 'ABC123 Corp.',
+        started: '2020-09-25T01:21:33.573Z',
+        employees: 20,
+        public: false
+    },
+    {
+        name: 'XYZ123 Corp.',
+        started: '2020-09-26T01:21:33.573Z',
+        employees: 12,
+        public: false
+    },
+    {
+        name: 'XYZ123 Corp.',
+        started: '2020-09-27T01:21:33.573Z',
+        employees: 12,
+        public: true
+    }
+];
+
+app.use(express.json());
+
+app.get('/organizations', (req, res) => {
+    const {
+        name,
+        started,
+        employees,
+        public
+    } = req.query;
+
+    // TODO: connect to real database query and response
+    const databaseResults = database.filter((entry) => true);
+
+    res.json({results: databaseResults});
+});
+
+app.post('/organizations', (req, res) => {
+    // TODO: update with real database success/failure handling
+    database.push(req.body);
+    res.status(200).json({results: database});
+});
+
+app.listen(port, () => { console.log(`listening on port: ${port}`); });
